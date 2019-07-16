b1a4d0ed95a9acbaaf0e0629a9c0e3f8e827b027
ANOOP P <anooppandikashala@gmail.com>
2019-07-16 23:37:33 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+from  CalcultorExample.CalculatorFunctions import *
+
+while(True):
+    numb1 = input("Enter Number 1 : ")
+    numb2 = input("Enter Number 2 : ")
+
+    print("\n**********Calculator Menu**********")
+    print("\t1 . Addition")
+    print("\t2 . Subtraction")
+    print("\t3 . Multiplication")
+    print("\t4 . Division")
+    print("\t5 . Quit")
+    print("***********************************")
+
+    choice = input("Enter your Choice :")
+
+    if(choice == 1):
+        result = addition(numb1,numb2)
+        print ("\tResult : "+str(result))
+    elif (choice == 2):
+        result = subtraction(numb1, numb2)
+        print ("\tResult : " + str(result))
+    elif (choice == 3):
+        result = multiplication(numb1, numb2)
+        print ("\tResult : " + str(result))
+    elif (choice == 4):
+        result = division(numb1, numb2)
+        print ("\tResult : " + str(result))
+    elif (choice == 5):
+        quit(0)
+    else:
+        print ("\tGive valid choice")
---
+++ CalcultorExample/CalculatorFunctions.py
@@ -0,0 +1,21 @@
+
+def addition(number1, number2):
+    result = number1 + number2
+    return result
+
+
+def subtraction(number1, number2):
+    result = number1 - number2
+    return result
+
+
+def multiplication(number1, number2):
+    result = number1 * number2
+    return result
+
+
+def division(number1, number2):
+    result = None
+    if (number2 != 0):
+        result = float(number1) / number2
+    return result
